0f31b85a73a7beea4fb56eae53207f39d5c01c4b
Pankaj <pankaj.10715354@ltimindtree.com>
2024-03-17 14:24:42 +0530

    Added sum.jav

---
+++ src/com/lti/test/Sum.java
@@ -0,0 +1,13 @@
+package com.lti.test;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c;
+		System.out.println(c=a+b);
+	}
+
+}
